240a5fc1f1de91bc345fbbb12d1e1bf16f7d2a24
Xi.Weicheng <Xi.Weicheng@emc.com>
2015-06-16 16:59:44 +0800

    init commit

---
+++ src/welcome.js
@@ -0,0 +1,16 @@
+import 'bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+
+export class Welcome{
+	heading = 'Welcome to the Aurelia Navigation App! http://aurelia.io/index.html#home';
+	firstName = 'Shelley';
+	lastName = 'Durniok';
+
+	get fullName(){
+		return `${this.firstName} ${this.lastName}`;
+	}
+
+	welcome(){
+		alert(`Welcome, ${this.fullName}!`);
+	}
+}
