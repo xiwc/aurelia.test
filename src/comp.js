0316151379efcd699819503b5daf230d53f455ca
weicheng xi <xiwc87@yeah.net>
2015-09-12 19:53:46 +0800

    ...

--- src/comp.js
+++ src/comp.js
@@ -1,38 +1,58 @@
 export class Comp {
 
+    logs = [];
+
     created(view) {
-        console.log("Comp created");
+        console.log(new Date().getTime() + " Comp created");
+        this.logs.push(new Date().getTime() + ' Comp created');
+        $.ntf.info(' Comp created');
     }
 
     bind(bindingContext) {
-        console.log("Comp bind");
+        console.log(new Date().getTime() + " Comp bind");
+        this.logs.push(new Date().getTime() + ' Comp bind');
+        $.ntf.info(' Comp bind');
     }
 
     unbind() {
-        console.log("Comp unbind");
+        console.log(new Date().getTime() + " Comp unbind");
+        this.logs.push(new Date().getTime() + ' Comp unbind');
+        $.ntf.info(' Comp unbind');
     }
 
     attached() {
-        console.log("Comp attached");
+        console.log(new Date().getTime() + " Comp attached");
+        this.logs.push(new Date().getTime() + ' Comp attached');
+        $.ntf.info(' Comp attached');
     }
 
     detached() {
-        console.log("Comp detached");
+        console.log(new Date().getTime() + " Comp detached");
+        this.logs.push(new Date().getTime() + ' Comp detached');
+        $.ntf.info(' Comp detached');
     }
 
     canActivate(params, routeConfig, navigationInstruction) {
         console.log('Comp canActivate');
+        this.logs.push(new Date().getTime() + ' Comp canActivate');
+        $.ntf.info(' Comp canActivate');
     }
 
     activate(params, routeConfig, navigationInstruction) {
         console.log('Comp activate');
+        this.logs.push(new Date().getTime() + ' Comp activate');
+        $.ntf.info(' Comp activate');
     }
 
     canDeactivate() {
         console.log('Comp canDeactivate');
+        this.logs.push(new Date().getTime() + ' Comp canDeactivate');
+        $.ntf.info(' Comp canDeactivate');
     }
 
     deactivate() {
         console.log('Comp deactivate');
+        this.logs.push(new Date().getTime() + ' Comp deactivate');
+        $.ntf.info(' Comp deactivate');
     }
 }
--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,38 +1,58 @@
 export class Lifecycle {
 
+    logs = [];
+
     created(view) {
-        console.log("Lifecycle created");
+        console.log(new Date().getTime() + ' Lifecycle created');
+        this.logs.push(new Date().getTime() + ' Lifecycle created');
+        $.ntf.info(' Lifecycle created');
     }
 
     bind(bindingContext) {
-        console.log("Lifecycle bind");
+        console.log(new Date().getTime() + ' Lifecycle bind');
+        this.logs.push(new Date().getTime() + ' Lifecycle bind');
+        $.ntf.info(' Lifecycle bind');
     }
 
     unbind() {
-        console.log("Lifecycle unbind");
+        console.log(new Date().getTime() + ' Lifecycle unbind');
+        this.logs.push(new Date().getTime() + ' Lifecycle unbind');
+        $.ntf.info(' Lifecycle unbind');
     }
 
     attached() {
-        console.log("Lifecycle attached");
+        console.log(new Date().getTime() + ' Lifecycle attached');
+        this.logs.push(new Date().getTime() + ' Lifecycle attached');
+        $.ntf.info(' Lifecycle attached');
     }
 
     detached() {
-        console.log("Lifecycle detached");
+        console.log(new Date().getTime() + ' Lifecycle detached');
+        this.logs.push(new Date().getTime() + ' Lifecycle detached');
+        $.ntf.info(' Lifecycle detached');
     }
 
     canActivate(params, routeConfig, navigationInstruction) {
-        console.log('Lifecycle canActivate');
+        console.log(new Date().getTime() + ' Lifecycle canActivate');
+        this.logs.push(new Date().getTime() + ' Lifecycle canActivate');
+        $.ntf.info(' Lifecycle canActivate');
     }
 
     activate(params, routeConfig, navigationInstruction) {
-        console.log('Lifecycle activate');
+        console.log(new Date().getTime() + ' Lifecycle activate');
+        this.logs.push(new Date().getTime() + ' Lifecycle activate');
+        $.ntf.info(' Lifecycle activate');
     }
 
     canDeactivate() {
-        console.log('Lifecycle canDeactivate');
+        console.log(new Date().getTime() + ' Lifecycle canDeactivate');
+        this.logs.push(new Date().getTime() + ' Lifecycle canDeactivate');
+        $.ntf.info(' Lifecycle canDeactivate');
     }
 
     deactivate() {
-        console.log('Lifecycle deactivate');
+        console.log(new Date().getTime() + ' Lifecycle deactivate');
+        this.logs.push(new Date().getTime() + ' Lifecycle deactivate');
+        $.ntf.info(' Lifecycle deactivate');
     }
 }
